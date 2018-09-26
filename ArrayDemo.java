f229dafaa30dad2e757d27304c45a00da1d253b1
grace-mao23 <gmao00@stuy.edu>
2018-09-26 18:51:59 -0400

    Fixed and tested; 0a works

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,18 +1,18 @@
 public class ArrayDemo{
   public static void printArray(int[]ary){
     System.out.print("[ ");
-    for (int i = 0; i < ary.length() - 1; i++) {
+    for (int i = 0; i < ary.length - 1; i++) {
       System.out.print(ary[i] + ", ");
     }
-    System.out.print(ary[ary.length - 1] + " ]")
+    System.out.print(ary[ary.length - 1] + " ]");
   }
 
 
 
   public static void main(String[] args){
-        int[] testA {
-            2, 3, 5, 7, 11, 13, 17, 19, 23
-        }
-        System.out.println(printArray(testA));
+    int[] testA = {
+      2, 3, 5, 7, 11, 13, 17, 19, 23
+    };
+    printArray(testA);    
   }
 }
